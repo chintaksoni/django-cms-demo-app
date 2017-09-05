3fd0f3ed1b1a7d0d3f91d54610dd9d69b8089109
--add <chintak.soni75@gmail.com>
2017-09-05 11:50:04 +0530

    django cms tutorial demo app

---
+++ polls/models.py
@@ -0,0 +1,29 @@
+#-*- coding: utf-8 -*-
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.choice_text
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User)
+    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    def __unicode__(self):
+        return self.title
